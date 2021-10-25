c4e5406870a91d868cdb9fb680ad8cef2bd23fbe
Ethan Schutzenhofer <ews624@mail.missouri.edu>
2021-10-24 22:39:39 -0500

    Possible Final Code Revision

--- RSA.c
+++ RSA.c
@@ -16,7 +16,6 @@ size_t save(FILE *ptr,mpz_t var){
 	check =mpz_out_str(ptr,10,var);
 	fprintf(ptr,"\n");
 	
-	
 	if(check == 0){
 		printf("Error saving file");
 		return 0;	
@@ -36,7 +35,7 @@ void file_save(char *filename, mpz_t var){
 	fclose(input_file);
 };
 
-void get_random(mpz_t random){
+void get_random(mpz_t random, char *k){
 	
 	gmp_randstate_t state;
 	gmp_randinit_mt(state);
@@ -46,16 +45,13 @@ void get_random(mpz_t random){
 	
 	int r =rand();
 	
-	//printf("R is :%d\n",r);
 	mpz_set_ui(seed,r);
 	gmp_randseed(state,seed);
 	
 	mp_bitcnt_t n;
-	n = 2048;
-	
+	n = *k;
 	
 	mpz_urandomb(random,state,n);
-	//mpz_out_str(stdout,10,random);
 	
 	int is_prime;
 	
@@ -63,10 +59,8 @@ void get_random(mpz_t random){
 	
 	while(is_prime == 0){
 		mpz_nextprime(random,random);
-		
 		is_prime = mpz_probab_prime_p(random,40);
 	}
-	
 
 };
 
@@ -79,16 +73,10 @@ char *concat(char *k){
 	
 	char a[2048];
 	a[0] = num;
-	
 	char b[2048] = "2";
 	char c[2048] = "0";
-	
-	printf("Dereferenced k:%s\n",k);
-	
-	
+		
 	strcat(l,k);
-	
-	printf("combined is:%s\n",l);
 	
 	strcat(a,l);
 	strcat(b,a);
@@ -99,12 +87,20 @@ char *concat(char *k){
 	ptr = c;
 	
 	return ptr;
+};
 
+void encrypt(mpz_t x,mpz_t k_large,mpz_t e, mpz_t N){
+	mpz_powm_sec(x,k_large,e,N);
+};
 
+void decrypt(mpz_t y,mpz_t x, mpz_t d, mpz_t N){
+	mpz_powm_sec(y,x,d,N);
 };
 
 int main (){
 	
+	
+	char file_name[64];
 	
 	mpz_t k_large;
 	mpz_t cat_large;
@@ -116,7 +112,7 @@ int main (){
 	
 	
 	char *k =(char*)malloc(1024*sizeof(char));
-	printf("Enter the value for k\n");
+	printf("Enter the value for k:");
 	scanf("%1023s",k);
 	
 	
@@ -162,14 +158,18 @@ int main (){
 	mpz_init(x);
 	mpz_init(y);
 	
+	mpz_t rop;
+	mpz_init(rop);
+	FILE *test_file = fopen("encrypt.txt","a+");
+	mpz_inp_str(e,test_file,10);
 	
-	get_random(p);
+	get_random(p,k);
 	
 	printf("P is :\n");
 	mpz_out_str(stdout,10,p);
 	printf("\n");
 	
-	get_random(q);
+	get_random(q,k);
 	
 	printf("Q is :\n");
 	mpz_out_str(stdout,10,q);
@@ -183,10 +183,7 @@ int main (){
 	mpz_init(p_minus);
 	mpz_init(q_minus);
 	
-	
 	mpz_sub_ui(p_minus,p,1);
-	
-
 	mpz_sub_ui(q_minus,q,1);
 	
 	printf("Q-1 is :\n");
@@ -204,10 +201,6 @@ int main (){
 	printf("\n");
 	
 	
-	
-	mpz_set_ui(e,65537);
-	
-	
 	mpz_invert(d,e,phi_n);
 	
 	printf("D is :\n");
@@ -223,23 +216,27 @@ int main (){
 	printf("\n");
 	
 	
+	//Load in x to be encrypted
+	FILE *x_file = fopen("X.txt","a+");
+	mpz_inp_str(x,x_file,10);
+	
 	
 	//Encrypt here
-	mpz_powm(x,k_large,e,N);
+	
+	encrypt(x,x,e,N);
+	
 	
 	printf("Encrypted X/Y is :\n");
 	mpz_out_str(stdout,10,x);
 	printf("\n");
 	
 	//Decrypt here
+	decrypt(y,x,d,N);
 	
-	mpz_powm(y,x,d,N);
 	
 	printf("Decrypted Y/X is :\n");
 	mpz_out_str(stdout,10,y);
 	printf("\n");
-	
-	
 	
 	
 	//PKCS encrypt
@@ -251,7 +248,7 @@ int main (){
 	mpz_out_str(stdout,10,cat_large);
 	printf("\n");
 	
-	mpz_powm(pkcs_x,cat_large,e,N);
+	encrypt(pkcs_x,cat_large,e,N);
 	
 	printf("Encrypted PKCS X/Y is :\n");
 	mpz_out_str(stdout,10,pkcs_x);
@@ -267,63 +264,21 @@ int main (){
 	mpz_out_str(stdout,10,pkcs_y);
 	printf("\n");
 	
-	
-	
-	int filesave;
-	
-	printf("Do you want to manually enter the output filenames? 1=Yes, 2=No\n");
-	scanf("%d",&filesave);
-	
-	if(filesave ==1){
-		char file_name[64];
-		printf("What do you want to name the file for e,p,q?\n");
-		scanf("%s",file_name);
-		file_save(file_name,e);
-		file_save(file_name,p);
-		file_save(file_name,q);
-		
-		printf("What do you want to name the file for d and N?\n");
-		scanf("%s",file_name);
-		file_save(file_name,N);
-		file_save(file_name,d);
-		
-		printf("What do you want to name the file for x to be encrypted?\n");
-		scanf("%s",file_name);
-		file_save(file_name,k_large);
-		
-		printf("What do you want to name the file for E(x)?\n");
-		scanf("%s",file_name);
-		file_save(file_name,x);
-		
-		
-		printf("What do you want to name the file for c to be decrypted?\n");
-		scanf("%s",file_name);
-		file_save("cipher.txt",x);
-		
-		
-		printf("What do you want to name the file for D(c)?\n");
-		scanf("%s",file_name);
-		file_save("decrypted.txt",y);
-		
-	}
-	else{
-		file_save("encrypt.txt",e);
-		file_save("encrypt.txt",p);
-		file_save("encrypt.txt",q);
 
-		file_save("private.txt",N);
-		file_save("private.txt",d);
-		
-		file_save("X.txt",k_large);
-		
-		file_save("encryptX.txt",x);
-		
-		file_save("cipher.txt",x);
-		
-		file_save("decrypted.txt",y);
-		
-	}
+	printf("What do you want to name the file for d and N?\n");
+	scanf("%s",file_name);
+	file_save(file_name,N);
+	file_save(file_name,d);
 	
+	
+	printf("What do you want to name the file for E(x)?\n");
+	scanf("%s",file_name);
+	file_save(file_name,x);
+	
+	
+	printf("What do you want to name the file for D(c)?\n");
+	scanf("%s",file_name);
+	file_save(file_name,y);
 	
 	
 	return 1;
